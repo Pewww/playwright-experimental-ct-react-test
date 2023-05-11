e76a7fdc6eb43612f147e84b0b2ff6d9527e36cb
정윤재[Front-End] <jung22337@naverz-corp.com>
2023-05-11 11:37:24 +0900

    test: react-datepicker (o)

---
+++ test/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DatePicker onChange={console.log} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
