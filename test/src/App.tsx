58a70643597eefdad9cec72a62a7eb4dbe5a4ac4
정윤재[Front-End] <jung22337@naverz-corp.com>
2023-05-11 14:32:34 +0900

    test: twin.macro (x)

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import tw, { styled } from 'twin.macro';
 
 function App() {
   return (
     <div className="App">
+      <Test>Hello</Test>
       <DatePicker onChange={console.log} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -27,3 +29,9 @@ function App() {
 }
 
 export default App;
+
+const Test = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: red;
+`;
